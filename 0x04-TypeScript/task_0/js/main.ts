865d7e66de454c170f10cab716717dc840ec9dfb
baby6969 <yafetzekarias23@fmail.com>
2025-09-14 17:35:25 -0400

    new file

--- 0x04-TypeScript/task_0/js/main.ts
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -57,3 +57,21 @@ studentsList.forEach(student => {
 // Add table to the DOM
 document.body.appendChild(table);
 
+interface Teacher {
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  location: string;
+  [key: string]: any;
+}
+
+const teacher3: Teacher = {
+  firstName: 'John',
+  lastName: 'Doe',
+  fullTimeEmployee: false,
+  location: 'London',
+  contract: false,
+};
+
+console.log(teacher3);
