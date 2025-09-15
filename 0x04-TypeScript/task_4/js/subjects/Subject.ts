603f794b0e4f22ed4f70426c7bf709c79b61f1b3
yafet zekariyas <93594782+baby6969@users.noreply.github.com>
2025-09-15 07:14:08 -0400

    Update Subject.ts

--- 0x04-TypeScript/task_4/js/subjects/Subject.ts
+++ 0x04-TypeScript/task_4/js/subjects/Subject.ts
@@ -1,8 +1,9 @@
 namespace Subjects {
+  // Interface Teacher should already exist in the namespace
   export class Subject {
     teacher!: Teacher; // instance attribute
 
-    // Explicitly typed setter for automated checks
+    // Correct setter method
     setTeacher(teacher: Teacher): void {
       this.teacher = teacher;
     }
